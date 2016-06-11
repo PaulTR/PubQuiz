9e06b3867661efe6e85915d5d3aafd5ee4c656c2
Mark DeVries <mark@radiantinteractive.com>
2016-06-11 09:22:53 -0600

    committing local changes

--- PubQuiz/mobile/src/main/java/com/avery/pubquiz/fragment/WinOrLose.java
+++ PubQuiz/mobile/src/main/java/com/avery/pubquiz/fragment/WinOrLose.java
@@ -1,9 +1,64 @@
 package com.avery.pubquiz.fragment;
 
-import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.avery.networking.nearby.Client;
+import com.avery.networking.nearby.messages.QuestionMessage;
+import com.avery.pubquiz.R;
 
 /**
  * Created by geoff on 6/10/16.
  */
 public class WinOrLose extends Fragment {
+
+    private static final String KEY_CLIENT = "key_client";
+    private static final String KEY_MESSAGE = "key_message";
+
+
+    public static WinOrLose getInstance(Client client, QuestionMessage message) {
+        Bundle args = new Bundle();
+        args.putSerializable(KEY_CLIENT, client);
+        args.putSerializable(KEY_MESSAGE, message);
+
+        WinOrLose winOrLose = new WinOrLose();
+        winOrLose.setArguments(args);
+
+        return winOrLose;
+    }
+
+
+    private TextView mWinLoseText;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_win_or_lose, container, false);
+
+        return v;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mWinLoseText = (TextView) view.findViewById(R.id.win_lose_text);
+
+        Bundle args = getArguments();
+        Client client = (Client) args.getSerializable(KEY_CLIENT);
+        QuestionMessage message = (QuestionMessage) args.getSerializable(KEY_MESSAGE);
+
+        if(client.getName().equalsIgnoreCase(message.questionType)) {
+            mWinLoseText.setText("Congratulations, You Win!");
+        }else {
+            mWinLoseText.setText("You Lose");
+        }
+    }
 }
