d90555620e61062166898caf96bd8d83b8a16c60
Paul Trebilcox-Ruiz <paultr@Pauls-MacBook-Pro.local>
2016-06-10 22:07:28 -0600

    added advertising test

--- PubQuiz/mobile/src/main/java/com/avery/pubquiz/MainActivity.java
+++ PubQuiz/mobile/src/main/java/com/avery/pubquiz/MainActivity.java
@@ -9,7 +9,7 @@ import com.avery.networking.nearby.NearbyDiscoveryCallback;
 import com.avery.networking.nearby.NearbyHostCallback;
 import com.avery.networking.nearby.NearbyManager;
 
-public class MainActivity extends AppCompatActivity implements NearbyHostCallback {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -29,68 +29,4 @@ public class MainActivity extends AppCompatActivity implements NearbyHostCallbac
         call.enqueue(new Callback<BeerList>() {
         */
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mManager.startDiscovery(30, new NearbyDiscoveryCallback() {
-            @Override
-            public void onDiscoveringSuccess() {
-                Log.e(TAG, "onDiscoverying success");
-            }
-
-            @Override
-            public void onDiscoveringFailed(int statusCode) {
-                Log.e(TAG, "onDiscovering failed : " + statusCode);
-            }
-
-            @Override
-            public void onEndpointLost(String endpointId) {
-                Log.e(TAG, "onEndpointLog : " + endpointId);
-            }
-
-            @Override
-            public void onEndpointFound(String endpointId, String endpointName) {
-                Log.e(TAG, "endpointid : " + endpointId + " : " + endpointName);
-            }
-        });
-
-        Log.e(TAG, "hello");
-
-        /*mManager.startAdvertising("averytv", new NearbyHostCallback() {
-            @Override
-            public void onAdvertisingSuccess() {
-                Log.e(TAG, "onadvertising success");
-            }
-
-            @Override
-            public void onAdvertisingFailed(int statusCode) {
-                Log.e(TAG, "onadvertising failed : " + statusCode);
-            }
-        });
-
-        */
-
-        NearbyManager.getInstance(this).startAdvertising("avery", this);
-    }
-
-    @Override
-    public void onAdvertisingSuccess() {
-
-    }
-
-    @Override
-    public void onAdvertisingFailed(int statusCode) {
-
-    }
-
-    @Override
-    public void onConnectionAccepted(Client client) {
-        Log.e(TAG, "onconnectionaccpeted : " + client.getName());
-    }
-
-    @Override
-    public void onConnectionFailed(int statusCode) {
-        Log.e(TAG, "on connection failed : " + statusCode);
-    }
 }
--- PubQuiz/tv/src/main/java/com/avery/pubquiz/fragments/FormTeamsFragment.java
+++ PubQuiz/tv/src/main/java/com/avery/pubquiz/fragments/FormTeamsFragment.java
@@ -3,13 +3,18 @@ package com.avery.pubquiz.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.avery.networking.nearby.Client;
+import com.avery.networking.nearby.NearbyHostCallback;
+import com.avery.networking.nearby.NearbyManager;
+import com.avery.networking.nearby.messages.BaseMessage;
 import com.avery.pubquiz.R;
 
-public class FormTeamsFragment extends Fragment {
+public class FormTeamsFragment extends Fragment implements NearbyHostCallback {
 
     public static FormTeamsFragment getInstance() {
         FormTeamsFragment fragment = new FormTeamsFragment();
@@ -27,6 +32,40 @@ public class FormTeamsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //initialize connecting teams
+        NearbyManager manager = NearbyManager.getInstance();
+
+        manager.setNearbyHostCallback(this);
+
+        manager.initialize(getActivity());
+    }
+
+    @Override
+    public void onConnectedSuccess() {
+        NearbyManager.getInstance().advertise();
+    }
+
+    @Override
+    public void onConnectedFailed() {
+        Log.e("Nearby", "onConnectedFailed");
+    }
+
+    @Override
+    public void onAdvertisingSuccess() {
+        Log.e("Nearby", "onAdvertisingSuccess");
+    }
+
+    @Override
+    public void onAdvertisingFailed(int statusCode) {
+        Log.e("Nearby", "onAdvertisingFailed: " + statusCode);
+    }
+
+    @Override
+    public void onConnectionAccepted(Client client, BaseMessage message) {
+        Log.e("Nearby", "onConnectionAccepted: " + message.messageType );
+    }
+
+    @Override
+    public void onConnectionFailed(int statusCode) {
+        Log.e("Nearby", "onConnectionFailed");
     }
 }
