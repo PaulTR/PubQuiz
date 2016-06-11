4807d3e47da522a8bcdfaa321469531691910eae
Paul Trebilcox-Ruiz <paultr@Pauls-MacBook-Pro.local>
2016-06-10 21:58:40 -0600

    form teams ui

--- PubQuiz/tv/src/main/java/com/avery/pubquiz/activities/MainActivity.java
+++ PubQuiz/tv/src/main/java/com/avery/pubquiz/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.avery.pubquiz.R;
+import com.avery.pubquiz.fragments.FormTeamsFragment;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +13,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initFragment();
+
+    }
+
+    private void initFragment() {
+        getFragmentManager().beginTransaction().replace(R.id.container, FormTeamsFragment.getInstance()).commit();
     }
 }
--- PubQuiz/tv/src/main/java/com/avery/pubquiz/fragments/FormTeamsFragment.java
+++ PubQuiz/tv/src/main/java/com/avery/pubquiz/fragments/FormTeamsFragment.java
@@ -1,6 +1,32 @@
 package com.avery.pubquiz.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.avery.pubquiz.R;
 
 public class FormTeamsFragment extends Fragment {
+
+    public static FormTeamsFragment getInstance() {
+        FormTeamsFragment fragment = new FormTeamsFragment();
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate( R.layout.fragment_form_teams, container, false );
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //initialize connecting teams
+    }
 }
